b788cf24e93651a86992edfcffa3f03fb0a12d08
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2021-08-30 11:13:51 +0200

    Packages & Server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Test");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
